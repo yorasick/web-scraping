cc19bda7c17cba5439af00cc351886c489ab8a7b
Yurii Kalishchuk <yurii@workerbase.com>
2024-11-20 23:40:30 +0200

    hw5: use sqlalchemy instead of sqlite

--- hw5/main.py
+++ hw5/main.py
@@ -3,6 +3,9 @@ import re
 import json
 import sqlite3
 
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 
 def get_html_content():
     url = 'https://www.lejobadequat.com/emplois'
@@ -19,19 +22,17 @@ def parse_vacancies(html_content):
     url_pattern = r'<a href="(.*?)".*class="jobCard_link"'
     urls: list[str] = re.findall(url_pattern, html_content)
 
-    print(urls)
-
     vacancies: list[dict] = [{ 'id': i+1, 'title': title.strip(), 'url': urls[i].strip() } for (i, title) in enumerate(titles)]
     
     return vacancies
 
 
-def save_to_json(filename: str, vacancies: list[dict]) -> None:
+def write_json(filename: str, vacancies: list[dict]) -> None:
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(vacancies, f, indent=4, ensure_ascii=False)
         
 
-def save_to_sqlite(filename: str, vacancies: list[dict]) -> None:
+def write_sqlite(filename: str, vacancies: list[dict]) -> None:
    
     conn = sqlite3.connect(filename)
     cursor = conn.cursor()
@@ -55,7 +56,7 @@ def save_to_sqlite(filename: str, vacancies: list[dict]) -> None:
     conn.close()
 
 
-def read_from_sqlite(filename: str) -> list[dict]:
+def read_sqlite(filename: str) -> list[dict]:
     conn = sqlite3.connect(filename)
     cursor = conn.cursor()
     vacancies = cursor.execute('SELECT * FROM vacancies').fetchall()
@@ -63,9 +64,52 @@ def read_from_sqlite(filename: str) -> list[dict]:
     return vacancies
 
 
+def write_sqlalchemy(filename: str, data: list[dict]) -> None:
+    engine = create_engine(f'sqlite:///{filename}')
+    Base = declarative_base()
+
+    class Vacancy(Base):
+        __tablename__ = 'vacancies'
+        id = Column(Integer, primary_key=True)
+        title = Column(String)
+        url = Column(String)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for vacancy in data:
+        session.merge(Vacancy(**vacancy), load=True)
+
+    session.commit()
+    session.close()
+
+
+def read_sqlalchemy(filename: str) -> list[dict]:
+    engine = create_engine(f'sqlite:///{filename}')
+    Base = declarative_base()
+
+    class Vacancy(Base):
+        __tablename__ = 'vacancies'
+        id = Column(Integer, primary_key=True)
+        title = Column(String)
+        url = Column(String)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    vacancies = session.query(Vacancy).all()
+
+    session.close()
+
+    return [vacancy.__dict__ for vacancy in vacancies]
+
 if __name__ == '__main__':
     html = get_html_content()
     vacancies = parse_vacancies(html)
-    save_to_json('output.json', vacancies)
-    save_to_sqlite('output.db', vacancies)
-    print(read_from_sqlite('output.db'))
+    write_json('output.json', vacancies)
+    # write_sqlite('output.db', vacancies)
+    # print(read_sqlite('output.db'))
+    write_sqlalchemy('output.db', vacancies)
+    print(read_sqlalchemy('output.db'))
