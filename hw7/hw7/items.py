f62a6efc199bc5e3eb0248d1135130a8dfb5f6cd
Yurii Kalishchuk <yurii@workerbase.com>
2024-11-27 00:46:22 +0200

    hw7: add pipeline to save into db

--- hw7/hw7/items.py
+++ hw7/hw7/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class Hw7Item(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class QuoteItem(scrapy.Item):
+    text = scrapy.Field()
+    author = scrapy.Field()
     pass
--- hw7/hw7/pipelines.py
+++ hw7/hw7/pipelines.py
@@ -6,8 +6,30 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class Hw7Pipeline:
+    def open_spider(self, spider):
+        filename = 'quotes.db'
+        self.connection = sqlite3.connect(filename)
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS quotes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                text TEXT NOT NULL,
+                author TEXT NOT NULL
+            )
+        """)
+        self.connection.commit()
+
+    
+    def close_spider(self, spider):
+        self.connection.close()
+
     def process_item(self, item, spider):
+        self.cursor.execute("""
+            INSERT INTO quotes (text, author) VALUES (?, ?)
+        """, (item['text'], item['author']))
+        self.connection.commit()
         return item
